6ebfd98072c6fa56c2528a10abd2ecbe5a859482
Mai Thi My Van <van.mai21@pn.com.vn>
2020-06-15 09:51:39 +0700

    cau5

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Room;
+
+class DashboardController extends Controller
+{
+    function create(){
+        return view("admin.create");
+    }
+    function store(Request $request ){
+        $name=$request->name;
+        $image=$request->file("image")->store("public");
+        //save full adress of image
+        $typeroom=$request->typeroom;
+        $number=$request->number;
+        $area=$request->area;
+        $price=$request->price;
+        //create new room with input value form
+        $room = new Room;
+        $room->name=$name;
+        $room->image=$image;
+        $room->typeroom=$typeroom;
+        $room->number=$number;
+        $room->area=$area;
+        $room->price=$price;
+        // store into database
+        $room->save();
+    }
+}
---
+++ resources/views/admin/create.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Add book</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      
+      <form action="/admin/create" method="POST" class="form-horizontal" role="form" enctype="multipart/form-data">
+      @csrf
+              <div class="form-group">
+                  <legend>Add room</legend>
+              </div>
+      
+              <div class="form-group">
+                  <div class="col-sm-10 col-sm-offset-2">
+                  <label for="">Name</label>
+                     <input type="text" name="name">
+                  </div>
+              </div>
+              <div class="form-group">
+                  <div class="col-sm-10 col-sm-offset-2">
+                  <label for="">Image</label>
+                     <input type="file" name="image">
+                  </div>
+              </div>
+              <div class="form-group">
+                  <div class="col-sm-10 col-sm-offset-2">
+                  <label for="">Type Room</label>
+                     <input type="text" name="typeroom">
+                  </div>
+              </div>
+              <div class="form-group">
+                  <div class="col-sm-10 col-sm-offset-2">
+                  <label for="">Number</label>
+                     <input type="text" name="number">
+                  </div>
+              </div>
+              <div class="form-group">
+                  <div class="col-sm-10 col-sm-offset-2">
+                  <label for="">Area</label>
+                     <input type="text" name="area">
+                  </div>
+              </div>
+              <div class="form-group">
+                  <div class="col-sm-10 col-sm-offset-2">
+                  <label for="">Price</label>
+                     <input type="text" name="price">
+                  </div>
+              </div>
+              
+              <div class="form-group">
+                  <div class="col-sm-10 col-sm-offset-2">
+                      <button type="submit" class="btn btn-primary">Add</button>
+                  </div>
+              </div>
+      </form>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
