a636a0d27d9c637761fd2a12998df5a7036671c6
Mai Thi My Van <van.mai21@pn.com.vn>
2020-06-15 09:04:13 +0700

    Cau 1 & 2

---
+++ database/migrations/2020_06_15_014605_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("image");
+            $table->string("typeroom");
+            $table->string("number");
+            $table->string("area");
+            $table->integer("price");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
---
+++ database/seeds/RoomSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Generator as Faker;
+
+class RoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0; $i<10; $i++){
+            DB::table("rooms")->insert([
+            "name"=>$faker->name, 
+            'image' => "",
+            "typeroom"=>"Phong family",
+            "number"=>"4 nguoi",
+            "area"=>"45m2",
+            "price"=>1200000
+            ]);
+        }
+    }
+}
